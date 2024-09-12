7722b14ec601739abdeeffe2f87f8b670641c680
alepaco <alepaco.maton@gmail.com>
2024-09-12 14:57:19 -0400

    Initial commit

---
+++ src/main/java/bo/digitalsignature/ApiRestDigitalSignatureApplication.java
@@ -0,0 +1,13 @@
+package bo.digitalsignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDigitalSignatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestDigitalSignatureApplication.class, args);
+    }
+
+}
